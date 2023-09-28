f3ca643757fd8ecad7ae1d1c33eebaa322c0391e
Nathan-88 <onwukaebuka88@gmail.com>
2023-09-28 11:14:57 +0100

    pascal triangle

--- 0x00-pascal_triangle/0-main.py
+++ 0x00-pascal_triangle/0-main.py
@@ -8,9 +8,8 @@ def print_triangle(triangle):
     """
     Print the triangle
     """
-    # for row in triangle:
-    #     print("[{}]".format(",".join([str(x) for x in row])))
-    print(triangle)
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
 
 
 if __name__ == "__main__":
